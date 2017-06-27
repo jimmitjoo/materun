1ad54763f0b3824bde6918a36d030c11e5283298
jimmitjoo <jimmitjoo@gmail.com>
2017-06-27 22:36:57 +0200

    =)

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,18 +1,32 @@
 @extends('layouts.mini')
 
 @section('content')
-    <div class="col-xs-12">
-        <h1>Villkor</h1>
+    <div class="row">
+        <div class="col-xs-12">
+            <h1>Villkor</h1>
 
-        <ul>
-            <li>Uppgifter du lämnar till oss hålls privata tills dess du önskar göra något publikt.</li>
-            <li>Vi kan komma att skicka e-post till dig med nyheter om tjänsten, t.ex. om vi lanserar skarpt, släpper nya funktioner, om någon går med i dina träningspass, eller om våra villkor uppdateras.</li>
-            <li>Dina uppgifter kommer inte säljas vidare till tredjepart.</li>
-        </ul>
+            <ul>
+                <li>Uppgifter du lämnar till oss hålls privata tills dess du önskar göra något publikt.</li>
+                <li>Vi kan komma att skicka e-post till dig med nyheter om tjänsten, t.ex. om vi lanserar skarpt,
+                    släpper nya funktioner, om någon går med i dina träningspass, eller om våra villkor uppdateras.
+                </li>
+                <li>Dina uppgifter kommer inte säljas vidare till tredjepart.</li>
+            </ul>
 
+        </div>
     </div>
-    <div class="col-xs-12">
-        <a class="btn btn-success btn-lg center-block" href="{{ route('register') }}">{{ __('user.create_account') }}</a>
+    <hr>
+    <div class="row">
+        <div class="col-xs-12">
+            <a class="btn btn-success btn-lg center-block"
+               href="{{ route('register') }}">{{ __('user.create_account') }}</a>
+        </div>
     </div>
-    
+    <hr>
+    <div class="row">
+        <div class="col-xs-12">
+            <a class="btn btn-default btn-lg center-block" href="{{ route('login') }}">{{ __('user.login') }}</a>
+        </div>
+    </div>
+
 @endsection
